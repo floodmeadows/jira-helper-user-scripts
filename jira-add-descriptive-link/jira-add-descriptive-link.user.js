dc60a80aee4b6b8627617d6989b5c734e44d5567
Andy <20927751+floodmeadows@users.noreply.github.com>
2022-02-23 14:36:29 +0000

    Changed format so issue key is first, then summary

--- jira-add-descriptive-link/jira-add-descriptive-link.user.js
+++ jira-add-descriptive-link/jira-add-descriptive-link.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @namespace    https://openuserjs.org/users/floodmeadows
 // @name         Jira add descriptive link
+// @namespace    https://openuserjs.org/users/floodmeadows
 // @description  Adds a copyable link with issue name and key, ready for pasting into messages etc.
 // @copyright    2021, floodmeadows (https://openuserjs.org/users/floodmeadows)
 // @license      MIT
-// @version      0.1.0
+// @version      0.2.0
 // @include      https://jira.*.uk/browse/*
 // @updateURL    https://openuserjs.org/meta/floodmeadows/jira-add-descriptive-link.meta.js
 // @downloadURL  https://openuserjs.org/src/scripts/floodmeadows/jira-add-descriptive-link.user.js
@@ -21,7 +21,7 @@ function addLink() {
     const issueKey = document.getElementById("key-val").childNodes[0].nodeValue;
     const issueName = document.getElementById("summary-val").childNodes[0].nodeValue;
     const newElement = document.createElement("div");
-    const h = '<a href="' + document.URL + '">\'' + issueName + '\' (' + issueKey + ')</a>';
+    const h = '<a href="' + document.URL + '">' + issueKey + ' (' + issueName + ')</a>';
     newElement.innerHTML = h;
     const target = document.getElementById('summary-val');
     target.parentNode.appendChild(newElement);
